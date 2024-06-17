919001cfe84839f8e2726bf7f16df919f9e8c048
adityaneupane <bijayneupane324@gmail.com>
2024-06-17 07:58:54 +0545

    new changes

--- src/Pages/UseEffecthook.jsx
+++ src/Pages/UseEffecthook.jsx
@@ -8,13 +8,28 @@ const UseEffecthook = () => {
     <>
     <h1>use effect hook</h1>
     
-    
-    
-    
-    
-    
     </>
   )
 }
 
 export default UseEffecthook
+
+
+
+
+
+
+// for alert messages 
+
+const UseEffecthook = () => {
+
+    useEffect( () => {
+        alert("hello");
+    }, []);
+
+    return (
+        <>
+        <h1>use effect hook</h1>
+        </>
+    )
+    export default UseEffecthook
